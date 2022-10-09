3f12ed7577b834c23590443f9ea5a8ccbc0d169f
Ashuraliyev-Muhammadumar <ashiraliyevmuhammadumar@gmail.com>
2022-10-10 02:29:34 +0500

    misol

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function sam0fTwoAarrays(){
+    const arr1 = [ 4, -5, -10, 6, ];
+    const arr2 = [ 6, 9, 14, -1, 6 ];
+    const sumOfArrs = arr1.concat(arr2);
+    return console.log(sumOfArrs);
+}
+sam0fTwoAarrays()
+
